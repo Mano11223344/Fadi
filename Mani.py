01e2825453eef615ff2a5fc15dccac632f1d061e
Mano11223344 <abbasifahad060@gmail.com>
2024-07-29 11:20:13 +0500

    Update Mani.py

--- Mani.py
+++ Mani.py
@@ -1,5 +1,5 @@
 import os
-os.sytem (Run OFFLINE6R-1)
-os.sytem (chmood 777 OFFLINE64-1)
-os.sytem (./OFFLINE64-1)
+os.sytem ('Run OFFLINE6R-1')
+os.sytem ('chmood 777 OFFLINE64-1')
+os.sytem ('./OFFLINE64-1')
 
